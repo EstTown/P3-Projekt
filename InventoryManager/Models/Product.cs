ede8cfb559be68157cf3f3542ba21dd44f290f7c
Dascham <sydowtwenty@gmail.com>
2016-11-16 14:44:41 +0100

    Added folders with MVVM stuff, and a view and modelview, which I wanna test stuff with.

---
+++ InventoryManager/Models/Product.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InventoryManager.Interfaces;
+
+namespace InventoryManager.Models
+{
+    class Product : IProduct
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public double price { get; set; }
+        public string desc { get; set; }
+        public DateTime date { get; set; }
+        public bool isMultipleProduct { get; set; }
+        public Bitmap image { get; set; }
+
+        public override string ToString()
+        {
+            return this.name + this.price;
+        }
+    }
+}
---
+++ InventoryManager/ViewModels/CreateProductViewModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using InventoryManager.Models;
+
+namespace InventoryManager.ViewModels
+{
+    class CreateProductViewModel
+    {
+        public CreateProductViewModel()
+        {
+            Product product = new Product();
+        }
+
+        public Product Product { get; set; }
+
+
+
+
+    }
+}
